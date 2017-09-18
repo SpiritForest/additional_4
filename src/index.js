0f73f92dba74ce5a274ca4902706c546ae9c2e87
SpiritForest <31858665+SpiritForest@users.noreply.github.com>
2017-09-18 13:32:31 +0300

    Add_4
    Add4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 
-module.exports = function multiply(first, second) {
-  var result = Number(first) * Number(second);
-  return +String(result);
+;module.exports = function multiply(first, second) {
+a = first.split('').reverse(); 
+b = second.split('').reverse(); 
+var result = [];
+
+for (var i = 0; a[i] >= 0; i++) { 
+for (var j = 0; b[j] >= 0; j++) { 
+	if (!result[i+j]) {result[i+j] = 0;}
+result[i + j] += a[i] * b[j]; 
+} 
+} 
+
+
+for (var i = 0; result[i] >= 0; i++){
+	if (!result[i+1]) break;
+result[i + 1] += Math.floor(result[i] / 10); 
+result[i] %= 10; 
+}
+
+
+var abs = result.reverse().join('');
+return  abs	; 
 }
